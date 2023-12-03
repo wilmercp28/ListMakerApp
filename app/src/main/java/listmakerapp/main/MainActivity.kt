5704456bb45199816a00db97ff9c51bab801480f
wilme <wilmercp28@gmail.com>
2023-12-02 21:45:06 -0500

    Build

--- app/src/main/java/listmakerapp/main/MainActivity.kt
+++ app/src/main/java/listmakerapp/main/MainActivity.kt
@@ -3,78 +3,46 @@ package listmakerapp.main
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.addCallback
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import listmakerapp.main.data.AppData
 import listmakerapp.main.data.getAppData
-import listmakerapp.main.data.saveAppData
-import listmakerapp.main.ui.composables.HomeScreen
-import listmakerapp.main.ui.composables.ListEditing
 import listmakerapp.main.ui.theme.ListMakerAppTheme
-import listmakerapp.main.viewmodel.ListViewModel
 
 class MainActivity : ComponentActivity() {
 
     private val dataStore: DataStore<Preferences> by preferencesDataStore(name = "AppState")
-    private val listViewModel: ListViewModel by viewModels()
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
         lifecycleScope.launch {
             val appData = getAppData(dataStore).first()
-            listViewModel.updateList(appData.list)
-            listViewModel.changeLoadingState()
+
         }
 
 
         setContent {
             val navController = rememberNavController()
-            val selectedIndex = remember { mutableIntStateOf(0) }
             ListMakerAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     NavHost(navController, startDestination = "HOME") {
-                        composable("HOME") {
-                            HomeScreen(navController, listViewModel, selectedIndex)
-                        }
-                        composable("EDIT") {
-                            ListEditing(navController, listViewModel, selectedIndex)
-                        }
+
                     }
                 }
-            }
-        }
-
-        onBackPressedDispatcher.addCallback(this) {
-            lifecycleScope.launch {
-                Log.d("Saved onCallBack", "Save")
-                saveDataAndFinish()
             }
         }
     }
@@ -87,10 +55,8 @@ class MainActivity : ComponentActivity() {
     }
 
     private suspend fun saveDataAndFinish() {
-        val appState = AppData(
-            list = listViewModel.list.value
-        )
-        saveAppData(appState, dataStore)
+
+
         Log.d("Saved", "Data saved before finish")
         finish()
     }
--- app/src/main/java/listmakerapp/main/data/Data.kt
+++ app/src/main/java/listmakerapp/main/data/Data.kt
@@ -1,14 +1,18 @@
 package listmakerapp.main.data
 
+import java.util.Date
 import java.util.UUID
 
 
 data class AppData(
-    val list: List<ListOfItems>
+    val list: List<ListOfItems>,
+    val selectedIndex: Int,
+    val isShoppingMode: Int
 )
 data class ListOfItems(
     var name: String,
-    var items: List<Item>
+    var items: List<Item>,
+    val dateOfCreation: Date
 )
 
 data class Item(
